0b32359d5b01e08ae12c79abd17eeafe0de277ab
kueda123 <kueda123@gmail.com>
2023-12-29 02:18:12 +0900

    リファクタリングの実施

--- shorter_name.py
+++ shorter_name.py
@@ -4,35 +4,86 @@ import re
 MAX_FILENAME_LENGTH = 255
 MP4_EXTENSION = ".mp4"
 DEFAULT_ENCODING = 'utf-8'
-MAX_CHAR_LEN = 3
 
 def get_bytes_length(filename, encoding=DEFAULT_ENCODING):
-    utf8_strings = filename.encode(encoding)
-    return len(utf8_strings)
+    """
+    Returns the byte length of the filename
+    """
+    the_bytes = filename.encode(encoding)
+    return len(the_bytes)
 
-def get_adjusted_filename(basename, extension=MP4_EXTENSION, footer="", encoding=DEFAULT_ENCODING):
-    #
-    # Cut off at the character boundary of Japanese UTF-8 Kanji characters
-    #
-    extension_bytes = get_bytes_length(extension)
-    footer_bytes = get_bytes_length(footer)
-    _max_bytes = MAX_FILENAME_LENGTH - extension_bytes - footer_bytes
- 
-    # Shave off the last positional character until 
-    # it is less than or equal to the value of _max_bytes.
-    exceeded_bytes = get_bytes_length(basename, encoding) - _max_bytes
+def has_4bytes_char(filename):
+    """
+    Search for 4-byte character strings
+    """
+    return re.findall(r"[\uD800-\uDBFF][\uDC00-\uDFFF]", filename)
+
+def get_max_char_bytes(basename):
+    """
+    Get the maximum length of the charactor in Japanese UTF-8 characters.
+    return:
+        4: If it contains a 4-byte string,
+        3: Others
+    """
+    # Get the maximum length of the charactor in Japanese UTF-8 characters.
+    return 4 if any(ord(c) >= 1280 for c in basename) else 3
+
+def truncate_filename_at_word_boundary(basename, max_basename_bytes):
+    """
+    Shorten the base name to the maximum character boundary
+    """
+    exceeded_bytes = get_bytes_length(basename) - max_basename_bytes
     if exceeded_bytes > 0:
-        basename = basename[:-(exceeded_bytes // MAX_CHAR_LEN) ]
-        while get_bytes_length(basename, encoding) > _max_bytes:
+        basename = basename[:-(exceeded_bytes//get_max_char_bytes(basename))]
+        while get_bytes_length(basename, encoding) > exceeded_bytes:
             basename = basename[:-1]
- 
+    return basename
+
+def remaining_bytes(footer, extension, encoding="utf-8"):
+    """
+    Returns the maximum remaining byte length
+
+    Args:
+        footer: Footer string
+        extension: Extension string
+        encoding: encoding
+
+    Returns:
+       Return byte length not already consumed by footer and extension
+    """
+    footer_bytes = footer.encode(encoding)
+    extension_bytes = extension.encode(encoding)
+    return MAX_FILENAME_LENGTH - len(footer_bytes) - len(extension_bytes)
+
+def get_adjusted_filename(basename: str, extension: str = MP4_EXTENSION, 
+                          footer: str = "", encoding: str = "utf-8") -> str:
+    """
+    Returns an adjusted filename that does not exceed the maximum length.
+    The filename is truncated at the word boundary if it exceeds the maximum length.
+
+    Args:
+        basename: The base name of the filename.
+        extension: The file extension.
+        footer: A footer to be added to the filename.
+        encoding: The encoding of the filename.
+
+    Returns:
+        The adjusted filename.
+    """
+    # Get the maximum length of the filename.
+    max_basename_bytes = remaining_bytes(footer, extension,  encoding)
+
+    # If the filename is longer than the maximum length, truncate it from the end.
+    basename = truncate_filename_at_word_boundary(basename, max_basename_bytes)
+
+    # Add the extension to the filename.
     return basename + footer + extension
+
 
 def rename_mp4_files(input_dir, output_dir):
     mp4_files = [
         os.path.join(input_dir, f) for f in os.listdir(input_dir) if f.endswith(MP4_EXTENSION)
     ]
-
 
     for path in mp4_files:
         dir, filename = os.path.split(path)
